40263199d58cb6ba8ce0cb1fe66243187f4b9808
qwerty36 <richard@lrtbl.nl>
2014-12-07 18:27:14 +0100

    afvink bonus

--- Afvinkopdracht 4.py
+++ Afvinkopdracht 4.py
@@ -25,9 +25,22 @@ at_total = adenine+thymine
 
 def main():
 	getComplement()
+	print (" ")
+	print ("##################################################")
+	print (" ")
 	GC_perc()
+	print (" ")
+	print ("###################################################")
+	print (" ")
 	AT_perc()
+	print (" ")
+	print ("####################################################")
+	print (" ")
 	isDNA()
+	print (" ")
+	print ("#####################################################")
+	print (" ")
+	translation()
 
 
 
@@ -72,6 +85,39 @@ def isDNA():
 def getComplement():
 	print (x.replace("a","T").replace("t","A").replace("c","G").replace("g","C").lower())
 
+###############################################################
+##Function to translate the sequence to a aminoacid sequence###
+###############################################################
+	
+def translation():
+        with open ("seq_afv4.txt", "r") as seq:
+                data=seq.readlines()
+
+        map = {"ttt":"F", "ttc":"F", "tta":"L", "ttg":"L",
+               "tct":"S", "tcc":"S", "tca":"S", "tcg":"S",
+               "tat":"Y", "tac":"Y", "taa":"STOP", "tag":"STOP",
+               "tgt":"C", "tgc":"C", "tga":"STOP", "tgg":"W",
+               "ctt":"L", "ctc":"L", "cta":"L", "ctg":"L",
+               "cct":"P", "ccc":"P", "cca":"P", "ccg":"P",
+               "cat":"H", "cac":"H", "caa":"Q", "cag":"Q",
+               "cgt":"R", "cgc":"R", "cga":"R", "cgg":"R",
+               "att":"I", "atc":"I", "ata":"I", "atg":"M/START",
+               "act":"T", "acc":"T", "aca":"T", "acg":"T",
+               "aat":"N", "aac":"N", "aaa":"K", "aag":"K",
+               "agt":"S", "agc":"S", "aga":"R", "agg":"R",
+               "gtt":"V", "gtc":"V", "gta":"V", "gtg":"V",
+               "gct":"A", "gcc":"A", "gca":"A", "gcg":"A",
+               "gat":"D", "gac":"D", "gaa":"E", "gag":"E",
+               "ggt":"G", "ggc":"G", "gga":"G", "ggg":"G",}
+
+        DNA=data[0].strip()
+        start = DNA.find('atg')
+        if start!= -1:
+            while start+2 < len(DNA):
+                codon = DNA[start:start+3]
+                if codon == "tag": break;
+                print(map[codon])
+                start+=3
 
 
 main()
--- TEST.py
+++ TEST.py
@@ -7,23 +7,23 @@ with open ("seq_afv4.txt", "r") as seq:
     data=seq.readlines()
 
 map = {"ttt":"F", "ttc":"F", "tta":"L", "ttg":"L",
-       "tct":"S", "tcc":"s", "tca":"S", "tcg":"S",
+       "tct":"S", "tcc":"S", "tca":"S", "tcg":"S",
        "tat":"Y", "tac":"Y", "taa":"STOP", "tag":"STOP",
-       "tgt":"c", "tgc":"c", "tga":"STOP", "tgg":"W",
+       "tgt":"C", "tgc":"C", "tga":"STOP", "tgg":"W",
        "ctt":"L", "ctc":"L", "cta":"L", "ctg":"L",
        "cct":"P", "ccc":"P", "cca":"P", "ccg":"P",
        "cat":"H", "cac":"H", "caa":"Q", "cag":"Q",
        "cgt":"R", "cgc":"R", "cga":"R", "cgg":"R",
-       "att":"I", "atc":"I", "ata":"I", "atg":"M",
+       "att":"I", "atc":"I", "ata":"I", "atg":"M/START",
        "act":"T", "acc":"T", "aca":"T", "acg":"T",
        "aat":"N", "aac":"N", "aaa":"K", "aag":"K",
        "agt":"S", "agc":"S", "aga":"R", "agg":"R",
        "gtt":"V", "gtc":"V", "gta":"V", "gtg":"V",
-       "gct":"A", "gcc":"a", "gca":"A", "gcg":"A",
+       "gct":"A", "gcc":"A", "gca":"A", "gcg":"A",
        "gat":"D", "gac":"D", "gaa":"E", "gag":"E",
        "ggt":"G", "ggc":"G", "gga":"G", "ggg":"G",}
 
-DNA=data[1].strip()
+DNA=data[0].strip()
 start = DNA.find('atg')
 if start!= -1:
     while start+2 < len(DNA):
